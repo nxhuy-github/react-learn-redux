c26e1ed2dfaa533ec340d48e2f668f77f43cbb14
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2019-09-18 17:14:31 +0200

    use useSelector, useDispatch, passing a variable in action

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux'
+import {increment, decrement} from './actions/index'
 
 function App() {
+  const counter = useSelector(state => state.counter) // get state from store
+  const isLogged = useSelector(state => state.isLogged) // get state from store
+  const dispatch = useDispatch()                      // this function helps us to execute a Action
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment(5))} >+</button>
+      <button onClick={() => dispatch(decrement())} >-</button>
+      {isLogged ? <h3>Valuable Information</h3> : <h3>Nothing</h3>}
     </div>
   );
 }
