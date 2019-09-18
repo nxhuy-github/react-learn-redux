3325a05bbf056a26f6c4c4bd3c2be438421c0910
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2019-09-18 16:49:34 +0200

    add redux-dev-tools on browser

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -6,7 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux'
 import allReducers from './reducers/index'
 
-const store = createStore(allReducers)
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
