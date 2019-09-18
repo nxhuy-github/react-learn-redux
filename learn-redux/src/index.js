89df26013f3c61e18fa28d3125a43998883fe174
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2019-09-18 16:53:09 +0200

    connect Store to the entire App via Provider

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -5,14 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux'
 import allReducers from './reducers/index'
+import {Provider} from 'react-redux' // connect store to app
 
-const store = createStore(
+const myStore = createStore(
   allReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={myStore} >
+    <App />
+  </Provider>, 
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
